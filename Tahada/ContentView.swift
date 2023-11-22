2efed21a01f783ce55c6b17c1a184fb301630da6
Hasna Ahmad <hassna.ahmad98@gmail.com>
2023-11-22 12:01:00 +0300

    Initial Commit

---
+++ Tahada/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Tahada
+//
+//  Created by Hasna Ahmed on 08/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Tahada/TahadaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TahadaApp.swift
+//  Tahada
+//
+//  Created by Hasna Ahmed on 08/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TahadaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
